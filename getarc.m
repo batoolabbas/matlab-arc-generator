17fee8321b166f944403ce72afb23457204767ee
batoolabbas <batoolabbas.91@gmail.com>
2018-10-04 21:14:02 -0400

    minor description update

--- getarc.m
+++ getarc.m
@@ -1,9 +1,9 @@
 function arc_points = getarc(r,as,c,axis_3D)
 %getarc: returns 2D points for an arc in (optionally) 3D space with specific orientation
 
-% Inputs: 
+% Input Parameters: 
 % r - radius of desired arc
-% as is a 1x2 vector containing start and ending angles (radians) for the arc
+% as - 1x2 vector containing start and ending angles (radians) for the arc
 % c - 1x3 vector containing the center for the arc in [x,y,z] format (can also be 1x2 [x,y])
 % axis_3D is the desired vector perpendicular to the surface of the circle
 % in 3D space that determines the 3D orientation of the arc
